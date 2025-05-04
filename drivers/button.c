9ec612adb5fc484d54837a523244850a5dd16a66
Hiroyuki OYAMA <oyama@module.jp>
2025-05-04 18:13:52 +0900

    Support for pico2 and pico2_w button input

--- drivers/button.c
+++ drivers/button.c
@@ -42,7 +42,14 @@ static bool __no_inline_not_in_flash_func(bootsel_button_raw)(void) {
                     GPIO_OVERRIDE_LOW << IO_QSPI_GPIO_QSPI_SS_CTRL_OEOVER_LSB,
                     IO_QSPI_GPIO_QSPI_SS_CTRL_OEOVER_BITS);
     for (volatile int i = 0; i < 1000; ++i);
-    bool button_state = !(sio_hw->gpio_hi_in & (1u << CS_PIN_INDEX));
+
+#if PICO_RP2040
+    #define CS_BIT (1u << 1)
+#else
+    #define CS_BIT SIO_GPIO_HI_IN_QSPI_CSN_BITS
+#endif
+    bool button_state = !(sio_hw->gpio_hi_in & CS_BIT);
+
     hw_write_masked(&ioqspi_hw->io[CS_PIN_INDEX].ctrl,
                     GPIO_OVERRIDE_NORMAL << IO_QSPI_GPIO_QSPI_SS_CTRL_OEOVER_LSB,
                     IO_QSPI_GPIO_QSPI_SS_CTRL_OEOVER_BITS);
